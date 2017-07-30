79f26a39de95955f991ca8578e587af5b036a717
taknado72 <taknado72@ukr.net>
2017-07-30 20:29:16 +0300

    add site

---
+++ library/mainFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+function loadPage($controllerName, $actionName = 'index'){
+    include_once PathPrefix . $controllerName . PathPostfix;
+    
+    $function = $actionName. 'Action';
+    $function();
+}
---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once '../config/config.php';            //Инициализация настроек
+include_once '../library/mainFunction.php';     //Основные функции
+
+//определяемс каким контроллером будем работать
+$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
+
+//определяем с какой функцией будем работать
+$actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+
+loadPage($controllerName, $actionName);
