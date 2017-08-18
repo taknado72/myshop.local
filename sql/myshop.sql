f64487344235c347001f356a7b86dc5ed2e4c8b0
taknado72 <taknado72@ukr.net>
2017-08-18 21:40:14 +0300

    обновлена база

--- sql/myshop.sql
+++ sql/myshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Авг 13 2017 г., 17:12
+-- Время создания: Авг 18 2017 г., 20:38
 -- Версия сервера: 10.1.21-MariaDB
 -- Версия PHP: 5.6.30
 
@@ -33,6 +33,48 @@ CREATE TABLE `categories` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `parent_id`, `name`) VALUES
+(1, 0, 'Телефоны'),
+(2, 0, 'Планшеты'),
+(3, 1, 'Телефоны Samsung'),
+(4, 1, 'Телефоны Apple'),
+(5, 2, 'Планшеты Apple'),
+(6, 2, 'Планшеты Acer'),
+(7, 2, 'Планшеты Samsung');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `price`, `image`, `status`) VALUES
+(1, 3, 'GT-S3500', 'Телефон GT-S3500', 3000, '1.jpg', 1),
+(2, 3, 'S-5670 Galaxy', 'Телефон S-5670 Galaxy', 7000, '2.jpg', 1),
+(3, 3, 'GT-E2600', 'Телефон GT-E2600', 5000, '3.jpg', 1),
+(4, 4, 'Apple 3S', 'Телефон Apple 3S', 20000, '4.jpg', 1),
+(5, 4, 'Apple 4S', 'Телефон Apple 4S', 22000, '5.jpg', 1),
+(6, 4, 'Apple 6S', 'Телефон Apple 6S', 23000, '6.jpg', 1),
+(7, 4, 'Apple 7S', 'Телефон Apple 7S', 24000, '7.jpg', 1);
+
+--
 -- Индексы сохранённых таблиц
 --
 
@@ -44,6 +86,12 @@ ALTER TABLE `categories`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -51,7 +99,12 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
